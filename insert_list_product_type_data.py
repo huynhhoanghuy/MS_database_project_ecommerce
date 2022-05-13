a0baea03134d8023143e59e06f2ac63b83615424
huynhhoanghuy <huy.hhoang@dmprof.com>
2022-05-13 20:50:47 +0700

    update insert product

--- insert_list_product_type_data.py
+++ insert_list_product_type_data.py
@@ -1,7 +1,6 @@
 
 list_product_type = [
-  { "_id": 1, "product_type_id": "s001S", "amount_with_size" : 70, "size": "small", "create_date": "01/01/2021/10/25/19", "update_date": "03/05/2022/11/21/00"},
-  { "_id": 2, "product_type_id": "s002S", "amount_with_size" : 40, "size": "small", "create_date": "02/01/2021/08/21/29", "update_date": "03/05/2022/15/31/10"},
-  { "_id": 3, "product_type_id": "c003L", "amount_with_size" : 20, "size": "large", "create_date": "01/01/2021/10/25/19", "update_date": "04/05/2022/09/30/00"},
-  { "_id": 4, "product_type_id": "c003M", "amount_with_size" : 10, "size": "medium", "create_date": "02/01/2021/12/21/11", "update_date": "04/05/2022/09/37/00"}
+  { "_id": 1, "product_type_id": "s001S", "product_type_name" : "snackOshi"},
+  { "_id": 2, "product_type_id": "c001L", "product_type_name" : "icMerio"},
+  { "_id": 3, "product_type_id": "s002S", "product_type_name" : "snackOshi"},
   ]
